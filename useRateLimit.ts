a65918e7d159590b8e65c0c1a524117b57cf7473
rikardbq <j10rikardb@gmail.com>
2026-02-16 16:30:18 +0100

    update ratelimiter to just use a regular time comparison instead of setTimeouts to reduce unwanted behaviours and complexity

--- useRateLimit.ts
+++ useRateLimit.ts
@@ -1,26 +1,14 @@
 import { useRef, useCallback } from "react";
 
 export const useRateLimit = () => {
-    const id = useRef<Function | undefined>(undefined);
-    const timer = useRef<number | undefined>(undefined);
-    const executeTimer = useRef<number | undefined>(undefined);
-    const rateLimiter = useCallback(
-        (cb: Function, time: number = 250) => {
-            clearTimeout(executeTimer.current);
-            executeTimer.current = setTimeout(() => {
-                id.current = undefined;
-                clearTimeout(timer.current);
-            }, time);
-
-            timer.current = setTimeout(() => {
-                if (id.current === cb) {
-                    cb();
-                }
-            }, time);
-            id.current = cb;
-        },
-        [],
-    );
+    const oldNow = useRef<number>(Date.now());
+    const rateLimiter = useCallback((cb: Function, time: number = 250) => {
+        const now = Date.now();
+        if (now > oldNow.current + time) {
+            oldNow.current = now;
+            cb();
+        }
+    }, []);
 
     return rateLimiter;
 };
